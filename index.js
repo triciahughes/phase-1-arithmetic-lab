2332530c49af8b8f98aee5080428e8454cc4b980
triciahughes <hughes.a.tricia@gmail.com>
2023-01-09 20:00:13 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+//assigning two variables
+const num1 = 2;
+const num2 = 31;
+//creating an equation and assigning it to a variable
+const multiply = num1 * num2;
+
+//generating a random number with an absolute min and max and assigning to a variable
+const random = Math.floor(Math.random() * 10) + 1;
+
+//assigning two variables
+const num3 = 200;
+const num4 = 98;
+//an equation that generates a reminder of 4
+const mod = num3 % num4;
+
+//returning 20 as the highest number with .max and assigning it to a variable
+const max = Math.max(2, 7, 18, 20);
